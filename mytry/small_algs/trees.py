5d5aeef5347119d76730676605e4ef4acb4b5d70
talex.maxim@pagaya.com <talex.maximpagaya.com@talex.maximpagaya.com-mac>
2022-11-13 10:07:50 +0200

    update

--- mytry/small_algs/trees.py
+++ mytry/small_algs/trees.py
@@ -346,5 +346,32 @@ def calculateBranchSums(node,sum, sums):
     calculateBranchSums(node.right,newSum, sums)    
 
 
+######################stack solution########################################
+def nodeDepths(root):
+    sum=0
+    stack = [{"node":root, "depth":0}]
+    while len(stack)>0:
+        nodeUnit = stack.pop()
+        node, depth = nodeUnit("node"), nodeUnit("depth")
+        if node is None:
+            continue
+        sum+=depth
+        stack.append({"node":node.left, "depth":depth + 1})
+        stack.append({"node":node.right, "depth": depth + 1})
+    return sum    
+###recursively
+def nodeDepths(root, depth=0):
+    if root is None:
+        return 0
+    return depth+nodeDepths(root.left, depth+1) + nodeDepths(root.right, depth +1)    
+
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
         
