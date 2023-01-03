7cea431d5641ac405236e999d8c325e71e3459f9
talexm <talexm@infibond.com>
2023-01-03 22:43:27 +0200

    new ads

--- mytry/small_algs/trees.py
+++ mytry/small_algs/trees.py
@@ -477,3 +477,26 @@ class MinHeap:
         self.siftUp(len(self.heap)-1, self.heap)
     def swap(self, i,j,heap):
         heap[i], heap[j] = heap[j], heap[i]
+
+##########################################################################################
+def invertBinaryTree(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapElements(current)
+        queue.append(current.left)
+        queue.append(current.rigth)
+
+def swapElements(tree):
+    tree.left, tree.right = tree.right, tree.left
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
